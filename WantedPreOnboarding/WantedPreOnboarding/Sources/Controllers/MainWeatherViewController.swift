9e86a5cae607bdead764e96d1b0640b6157da84a
yoonju lee <rsyjlee@gmail.com>
2022-06-12 17:18:45 +0900

    feat: Main ViewController와 TableView Datasource 연결
    * tableView의 상태와 데이터를 나타낼 State enum 구현

--- WantedPreOnboarding/WantedPreOnboarding/Sources/Controllers/MainWeatherViewController.swift
+++ WantedPreOnboarding/WantedPreOnboarding/Sources/Controllers/MainWeatherViewController.swift
@@ -8,10 +8,46 @@
 import UIKit
 
 class MainWeatherViewController: UIViewController {
-
+    
+    // MARK: IBOutlet
+    
     @IBOutlet weak var weatherTableView: UITableView!
 
+    // MARK: TableView state
+
+    enum State {
+        case loading
+        case populated([CurrentWeatherData])
+        case empty
+        case error(Error)
+        
+        var weatherDatas: [CurrentWeatherData] {
+            switch self {
+            case .populated(let weatherDatas):
+                return weatherDatas
+            default:
+                return []
+            }
+        }
+    }
+
+    // MARK: Properties
+    
+    private var tableViewDatasource: WeatherTableViewDatasource?
+    private var state: State = .loading
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTableViewDelegate()
+    }
+}
+
+// MARK: - TableView configuring method
+
+extension MainWeatherViewController {
+    
+    func setTableViewDelegate() {
+        tableViewDatasource = WeatherTableViewDatasource(with: state.weatherDatas)
+        weatherTableView.dataSource = tableViewDatasource
     }
 }
