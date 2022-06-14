0f7fdb58ccec6fb7dd0318b6f5b443898adc7da8
yoonju lee <rsyjlee@gmail.com>
2022-06-14 13:37:38 +0900

    feat: DetailView의 컨텐츠 세팅

--- WantedPreOnboarding/WantedPreOnboarding/Sources/Controllers/DetailWeatherViewController.swift
+++ WantedPreOnboarding/WantedPreOnboarding/Sources/Controllers/DetailWeatherViewController.swift
@@ -25,10 +25,116 @@ class DetailWeatherViewController: UIViewController {
     // MARK: Properties
 
     var currentWeather: CurrentWeather?
+    private var iconImageUseCase: IconImageUseCaseType?
 
     // MARK: View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureContents()
     }
 }
+
+// MARK: - View configuring method
+
+extension DetailWeatherViewController {
+    
+    private func configureContents() {
+        configureIconImage()
+        configureLabels()
+    }
+    
+    private func configureLabels() {
+        guard let currentWeather = currentWeather else {
+            return
+        }
+        
+        let currentTemperature = converToCelsius(from: currentWeather.information.temperature)
+        let maximumTemperature = converToCelsius(from: currentWeather.information.maximumTemperature)
+        let minimumTemperature = converToCelsius(from: currentWeather.information.minimumTemperature)
+        let windChill = converToCelsius(from: currentWeather.information.windChill)
+        
+        cityNameLabel.text = convertToKorean(from: currentWeather.cityName)
+        descriptionLabel.text = currentWeather.weather[0].description
+        
+        currentTemperatureLabel.text = "\(currentTemperature)℃"
+        maximumTemperatureLabel.text = "\(maximumTemperature)℃"
+        minimumTemperatureLabel.text = "\(minimumTemperature)℃"
+        windChillLabel.text = "\(windChill)℃"
+        humidityLabel.text = currentWeather.information.humidity.description
+        windSpeedLabel.text = currentWeather.wind.speed.description
+        pressureLabel.text = currentWeather.information.pressure.description
+    }
+    
+    private func configureIconImage() {
+        iconImageUseCase = IconImageUseCase()
+        
+        if let currentWeather = currentWeather {
+            iconImageUseCase?.fetchThumbnail(
+                of: currentWeather.weather[0].iconID
+            ) { [weak self] result in
+                switch result {
+                case .success(let image):
+                    DispatchQueue.main.async {
+                        self?.iconImageView.image = image
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+        
+    }
+
+    private func converToCelsius(from kelvin: Double) -> Int {
+        return Int(kelvin - 273.15)
+    }
+
+    private func convertToKorean(from cityName: String) -> String {
+        switch cityName {
+        case "Gongju":
+            return "공주"
+        case "Gwangju":
+            return "광주"
+        case "Gumi":
+            return "구미"
+        case "Gunsan":
+            return "군산"
+        case "Daegu":
+            return "대구"
+        case "Daejeon":
+            return "대전"
+        case "Mokpo":
+            return "목포"
+        case "Busan":
+            return "부산"
+        case "Seosan City":
+            return "서산"
+        case "Seoul":
+            return "서울"
+        case "Sokcho":
+            return "속초"
+        case "Suwon-si":
+            return "수원"
+        case "Suncheon":
+            return "순천"
+        case "Ulsan":
+            return "울산"
+        case "Iksan":
+            return "익산"
+        case "Jeonju":
+            return "전주"
+        case "Jeju City":
+            return "제주"
+        case "Cheonan":
+            return "천안"
+        case "Chungju":
+            return "충주"
+        case "Chuncheon":
+            return "춘천"
+        default:
+            return "진행중"
+        }
+    }
+}
+
